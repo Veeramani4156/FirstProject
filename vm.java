720f6567d9d398aa168961d5917a850976715cfb
Veeramani4156 <veeramani4156@gmail.com>
2022-11-24 17:24:17 +0530

    back to basic

--- vm.java
+++ vm.java
@@ -1,5 +1,5 @@
 class student{
-    private String name;
+    private String name = "Veeramani_VM";
     private int age = 22;
     private int rollnum = 175157;
     public String getName() {
@@ -24,10 +24,10 @@ class student{
 public class vm {
     public static void main(String[] args) {
         student s1 = new student();
-        s1.setName("veeramani");
+        //s1.setName("veeramani");
         //s1.setAge(23);
         //s1.setRollnum(2269949);
-        //System.out.println(s1.getName());
+        System.out.println(s1.getName());
         System.out.println(s1.getAge());
         System.out.println(s1.getRollnum());
 
