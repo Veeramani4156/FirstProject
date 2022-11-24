1052cede15d0b116b8ea751ad5de36d6dfa212f2
Veeramani4156 <veeramani4156@gmail.com>
2022-11-24 16:16:04 +0530

    variables changed

--- vm.java
+++ vm.java
@@ -2,7 +2,7 @@ public class vm{
     
     public static void main(String[] args){
         int i = 12;
-        int j = 13;
+        int j = 34;
         System.out.println(i + j);
-    } 
+    }
 }
