90a44718dd0a1d1308d40ebdb1419e7758771bdf
Veeramani4156 <veeramani4156@gmail.com>
2022-11-24 17:15:08 +0530

    initial added

--- vm.java
+++ vm.java
@@ -1,9 +1,36 @@
-public class vm{
-    
-    public static void main(String[] args){
-        int i = 34;
-        int j = 34;
-        int k = 322;
-        System.out.println(i + j + k);
+class student{
+    private String name = "veeramani_VM";
+    private int age = 22;
+    private int rollnum = 175157;
+    public String getName() {
+        return name;
     }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public int getRollnum(){
+        return rollnum;
+    }
+    public void setRollnum(int rollnum){
+        this.rollnum = rollnum;
+    }
+}
+public class vm {
+    public static void main(String[] args) {
+        student s1 = new student();
+        //s1.setName("veeramani");
+        //s1.setAge(23);
+        //s1.setRollnum(2269949);
+        System.out.println(s1.getName());
+        System.out.println(s1.getAge());
+        System.out.println(s1.getRollnum());
+
+    }
+
 }
